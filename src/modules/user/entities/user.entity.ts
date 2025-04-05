831c654268b978e1c9ce1cfae9a9c3e9a0fa4deb
Alireza Zamani <alirezazamanidev80@gmail.com>
2025-04-05 21:11:40 +0330

    feat: create user entity

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('user')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ name: 'full_name', type: 'varchar', length: 100 })
+    fullName: string;
+
+    @Column({ unique: true, type: 'varchar', length: 50 })
+    username: string;
+
+    @Column({ name: 'hash_password', type: 'varchar' })
+    hashPassword: string;
+    @CreateDateColumn({ name: 'created_at' })
+    created_at: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updated_at: Date;
+}
